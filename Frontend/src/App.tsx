8d8ea6dbffb8a600e2498e6cd9906da06a17f899
Halloloid <animation12321567@gmail.com>
2026-02-08 16:56:41 +0530

    Tailwind shadcnui setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Button } from "./components/ui/button"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className="flex min-h-svh flex-col items-center justify-center " >
+      <Button>Click me</Button>
+    </div>
+    </>
   )
 }
 
