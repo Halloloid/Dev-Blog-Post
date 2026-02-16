08af83dceaeb65fe89ff3c64bc88f6ddb9b4d800
Halloloid <animation12321567@gmail.com>
2026-02-16 21:06:22 +0530

    add components in Home

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,21 +1,27 @@
 import Card from "@/components/Card"
 import SearchBar from "@/components/SearchBar"
+import { MorphingText } from "@/components/ui/morphing-text"
+import { ShimmerButton } from "@/components/ui/shimmer-button"
 
 const Home = () => {
   return (
     <>
     <div>
-      <nav>aa</nav>
+      <nav className="w-full h-15 mb-5 bg-vio p-2 rounded-b-xl">
+          <MorphingText className="-ms-80 " texts={["Dev","Blog","Post"]}/>
+      </nav>
       <div className="grid grid-cols-10">
-        <div className="-ms-125 col-span-7">
+        <div className="-ms-100 col-span-7">
           <SearchBar/>
         </div>
-        <div>
-          hsjfhjfh
+        <div className="flex w-max gap-7 -ms-80">
+          <ShimmerButton>Most Liked</ShimmerButton>
+          <ShimmerButton>Most Viewed</ShimmerButton>
+          <ShimmerButton>Recent</ShimmerButton>
         </div>
       </div>
-    <div className="grid grid-cols-10">
-      <div className="col-span-4">
+    <div className="grid grid-cols-10 mt-10">
+      <div className="col-span-3">
         jdjsjd
       </div>
       <div className="col-span-6">
