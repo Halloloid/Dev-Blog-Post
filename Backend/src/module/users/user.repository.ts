4389204a33c8afe966d7c8c8838faac003c1c97b
Halloloid <animation12321567@gmail.com>
2026-03-13 19:31:35 +0530

    Added Create USername API

--- Backend/src/module/users/user.repository.ts
+++ Backend/src/module/users/user.repository.ts
@@ -51,3 +51,30 @@ export const publicUserProfile = async(req:Request,res:Response) => {
         res.status(500).json({message:"Server Error"})
     }
 }
+
+export const createUsername = async(req:Request,res:Response) => {
+    try {
+        const user = req.user as {sub:string}
+        if(!user?.sub) return res.status(401).json({message:"Unauthorized"});
+
+        const username = req.body.username?.trim().toLowerCase();
+
+        if(!username || !/^[a-z0-9._]{3,20}$/.test(username)){
+            return res.status(400).json({message:"Invalid Username"})
+        }
+
+        await prisma.user.update({
+            where:{id:user.sub},
+            data:{
+                user_name:username
+            }
+        })
+        return res.status(200).json({message:`User name is :${username}`})
+        
+
+    } catch (error:any) {
+        if(error.code === "P2002") return res.status(400).json({message:"Username already taken"});
+        console.error(error);
+        res.status(500).json({message:"Server Error"})
+    }
+}
--- Backend/src/module/users/users.routes.ts
+++ Backend/src/module/users/users.routes.ts
@@ -1,8 +1,10 @@
 import express from "express"
-import { publicUserProfile } from "./user.repository.js"
+import { createUsername, publicUserProfile } from "./user.repository.js"
+import { authMiddleware } from "../../middlewares/auth.middleware.js"
 
 const userRoutes = express.Router()
 
 userRoutes.get("/:username",publicUserProfile)
+userRoutes.post("/create",authMiddleware,createUsername)
 
 export default userRoutes
